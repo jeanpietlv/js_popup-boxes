f79de0514f9b6ba47c5d9db8536749ebe1796312
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-01-17 15:17:28 +0100

    push

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+window.alert("Hallo Welt");
+var name1 = window.prompt("Bitte geben Sie Ihren Namen ein.");
+var name2 = window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+var meinung = window.confirm("Stimmen Sie meiner Meinung zu?");
+
+console.log(name1);
+console.log(name2);
+console.log(meinung);
+
+
+let b = 5;
+let a = b * 5 - 3;
+let msg = "ZU VERZEHRENDE ENERGIEDRINKS HEUTE:"
+alert(msg + a);
+console.log(msg + a);
+
